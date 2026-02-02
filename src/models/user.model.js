4d994ec31b139ba59ea4f33ced6edd95d1f1694f
myusercloud <lucasharrizon@gmail.com>
2026-02-02 18:29:49 +0300

    solid reg

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import pool from "../config/db.js";
 
+
+// Create new user
 export const createUser = async (email, hashedPassword) => {
   const query = `
     INSERT INTO users (email, password)
@@ -10,6 +12,8 @@ export const createUser = async (email, hashedPassword) => {
   return rows[0];
 };
 
+
+// Find user by email
 export const findUserByEmail = async (email) => {
   const query = `SELECT * FROM users WHERE email = $1`;
   const { rows } = await pool.query(query, [email]);
